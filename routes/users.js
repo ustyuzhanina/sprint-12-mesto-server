b69a9c1087b905aadb7cbf4c2f6ec54a34c0cdb2
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2020-08-04 23:50:29 +0700

    First working version of sprint-12 project

---
+++ routes/users.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+//const { createReadStream } = require('fs'); next time
+
+
+router.get('/', (req, res) => {
+  const filePath = path.join(__dirname, '../data', 'users.json');
+  const reader = fs.createReadStream(filePath);
+
+  reader.on('error', err => res.status(500).send({'message': err.message}));
+
+  reader.on('open', () => {
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    reader.pipe(res);
+  });
+
+});
+
+
+router.get('/:id', (req, res) => {
+  const filePath = path.join(__dirname, '../data', 'users.json');
+  const reader = fs.createReadStream(filePath, {encoding: 'utf8'});
+
+  reader.on('error', err => res.status(500).send({'message': err.message}));
+
+  let array = '';
+
+  reader.on('data', (chunk) => {
+    array += chunk.toString();
+  });
+
+    reader.on('end', () => {
+      const arrayParsed = JSON.parse(array);
+      const user = arrayParsed.find((user) => String(user._id) === req.params.id);
+
+      if(!user) {
+        res.status(404).send({ "message": "Нет пользователя с таким id" });
+        return;
+      }
+
+      res.send(user);
+    })
+
+
+
+});
+
+module.exports = router;
