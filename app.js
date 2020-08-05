1f6674e601f353caf84828238df448dcb5ee2350
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2020-08-06 01:01:19 +0700

    Added missing DevDependancies, corrected lint errors, eliminated lacking server crashing cases

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-
 
 const cards = require('./routes/cards.js');
 const users = require('./routes/users.js');
-
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public'))); // теперь клиент имеет доступ только к публичным файлам
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/cards', cards);
 
@@ -19,7 +16,7 @@ app.use('/users', users);
 
 app.use((req, res) => {
   res.status('404');
-  res.send({ "message": "Запрашиваемый ресурс не найден" });
+  res.send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 app.listen(PORT);
--- routes/cards.js
+++ routes/cards.js
@@ -2,20 +2,30 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 
-
-
 router.get('/', (req, res) => {
   const filePath = path.join(__dirname, '../data', 'cards.json');
 
   const reader = fs.createReadStream(filePath);
 
-  reader.on('error', err => res.status(500).send({'message': err.message}));
+  reader.on('error', (err) => res.status(500).send({ message: err.message }));
 
-  reader.on('open', () => {
-    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-    reader.pipe(res);
+  let data = '';
+
+  reader.on('data', (chunk) => {
+    data += chunk.toString();
   });
 
+  reader.on('end', () => {
+    try {
+      JSON.parse(data);
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
+
+    const dataParsed = JSON.parse(data);
+
+    res.send(dataParsed);
+  });
 });
 
 module.exports = router;
